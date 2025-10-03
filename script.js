1ff3a634b57d22c45cfb2d689faa92100f45c286
gaurav-geeky <gauravmpreat@gmail.com>
2025-10-03 19:26:27 +0530

    spotify 1st push

---
+++ script.js
@@ -0,0 +1,8 @@
+console.log('Lets write javascript');
+
+async function main() {
+    let a = await fetch("http://127.0.0.1:5500/songs/");
+    let response = await a.text();
+    console.log(response)
+} 
+main(); 
